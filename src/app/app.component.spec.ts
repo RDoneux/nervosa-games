fb54dcedbd76051871e6318d3d0d34975ed616e8
RDoneux <robertdoneux@gmail.com>
2023-09-16 20:26:58 +0100

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('nervosa-games');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nervosa-games app is running!');
-  });
 });
