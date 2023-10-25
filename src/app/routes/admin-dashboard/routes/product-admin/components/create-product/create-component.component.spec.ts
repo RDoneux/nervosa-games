c5573ba349fd281ca3e63203112b876306520a55
RDoneux <robertdoneux@gmail.com>
2023-10-25 09:19:31 +0100

    create product component tests

--- src/app/routes/admin-dashboard/routes/product-admin/components/create-product/create-component.component.spec.ts
+++ src/app/routes/admin-dashboard/routes/product-admin/components/create-product/create-component.component.spec.ts
@@ -2,26 +2,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateComponentComponent } from './create-component.component';
 import { ProductAdminModule } from '../../product-admin.module';
-import { getFirestoreStub } from 'src/app/services/firestore/firestore-testing';
-import { FirestoreService } from 'src/app/services/firestore/firestore.service';
 import { getStorageStub } from 'src/app/services/cloud-storage/storage-testing';
 import { StorageService } from 'src/app/services/cloud-storage/storage.service';
+import { CreateProductService } from '../../services/create-product.service';
+import { mockedProduct } from 'src/app/data/test-data.spec';
 
 describe('CreateComponentComponent', () => {
   let component: CreateComponentComponent;
   let fixture: ComponentFixture<CreateComponentComponent>;
 
-  let firestoreServiceMock: any;
   let firestoreStorageMock: any;
 
+  let createProductServiceMock: jasmine.SpyObj<CreateProductService>;
+
   beforeEach(() => {
-    firestoreServiceMock = getFirestoreStub('');
     firestoreStorageMock = getStorageStub('');
+    createProductServiceMock = jasmine.createSpyObj('CreateProductService', [
+      'uploadNewProduct',
+    ]);
     TestBed.configureTestingModule({
       imports: [ProductAdminModule],
       providers: [
-        { provide: FirestoreService, useValue: firestoreServiceMock },
-        { provide: StorageService, useValue: firestoreServiceMock },
+        { provide: CreateProductService, useValue: createProductServiceMock },
+        { provide: StorageService, useValue: firestoreStorageMock },
       ],
     });
     fixture = TestBed.createComponent(CreateComponentComponent);
@@ -31,5 +34,54 @@ describe('CreateComponentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#addDownloadUrl', () => {
+    it('should set product.imageUrl to specified string', () => {
+      component.addDownloadUrl('test-image-url');
+
+      expect(component.product.imageUrl).toEqual('test-image-url');
+    });
+  });
+
+  describe('#updateTags', () => {
+    it('should set product.tags to specified string array', () => {
+      component.updateTags(['test-tag-one', 'test-tag-two']);
+
+      expect(component.product.tags).toEqual(['test-tag-one', 'test-tag-two']);
+    });
+  });
+
+  describe('#onSubmit', () => {
+    it('should populate product.dateUploaded', () => {
+      component.onSubmit();
+
+      expect(component.product.dateUploaded).toBeDefined();
+    });
+    it('should call createProductService #uploadNewProduct with mocked product', () => {
+      component.product = mockedProduct;
+
+      component.onSubmit();
+
+      expect(
+        createProductServiceMock.uploadNewProduct
+      ).toHaveBeenCalledOnceWith(mockedProduct);
+    });
+    it('should reset product after submit', () => {
+      component.product = mockedProduct;
+
+      component.onSubmit();
+
+      expect(component.product.imageUrl).toEqual('');
+      expect(component.product.title).toEqual('');
+      expect(component.product.itemNumber).toEqual(0);
+      expect(component.product.price).toEqual(0);
+      expect(component.product.tags).toEqual([]);
+      expect(component.product.description).toEqual('');
+
+      expect(component.product.id).toBeDefined();
+      expect(component.product.dateUploaded).toBeDefined();
+      expect(component.product.isLiked).toBeFalse();
+    });
   });
 });
