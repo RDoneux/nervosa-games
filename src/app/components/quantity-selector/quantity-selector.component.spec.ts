47cdf21715e84a2eb7854c98a3f7f5c5aa0f6845
RDoneux <robertdoneux@gmail.com>
2023-11-06 17:58:54 +0000

    quantity selector tests

--- src/app/components/quantity-selector/quantity-selector.component.spec.ts
+++ src/app/components/quantity-selector/quantity-selector.component.spec.ts
@@ -18,4 +18,34 @@ describe('QuantitySelectorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('#onAdd', () => {
+    it('should increment current value and emit new value', () => {
+      spyOn(component.valueChanged, 'emit');
+      component.value = 1;
+
+      component.onAdd();
+
+      expect(component.valueChanged.emit).toHaveBeenCalledOnceWith(2);
+    });
+  });
+
+  describe('#onRemove', () => {
+    it('should do nothing if current value is 0', () => {
+      spyOn(component.valueChanged, 'emit');
+      component.value = 0;
+
+      component.onRemove();
+
+      expect(component.valueChanged.emit).not.toHaveBeenCalled();
+    });
+    it('should decrement value and emit new value if current value is greater than 0', () => {
+      spyOn(component.valueChanged, 'emit');
+      component.value = 3;
+
+      component.onRemove();
+
+      expect(component.valueChanged.emit).toHaveBeenCalledOnceWith(2);
+    });
+  });
 });
