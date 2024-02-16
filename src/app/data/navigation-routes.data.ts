ea9cdbbac8e33d5ffbe8d0aed8cecfe02ead9357
Rob Doneux <robertdoneux@gmail.com>
2024-02-16 13:52:05 +0000

    removes services and games from navigation

--- src/app/data/navigation-routes.data.ts
+++ src/app/data/navigation-routes.data.ts
@@ -2,8 +2,8 @@ import { INavigationRoute } from '../components/top-navigation/interfaces/i-navi
 
 export const nervosaGamesSiteRoutes: INavigationRoute[] = [
   { label: 'Shop', route: '/shop' },
-  { label: 'Services', route: '/services' },
+  // { label: 'Services', route: '/services' },
   { label: 'News', route: '/news' },
-  { label: 'Games', route: '/games' },
+  // { label: 'Games', route: '/games' },
   { label: 'About Us', route: '/about-us' },
 ];
