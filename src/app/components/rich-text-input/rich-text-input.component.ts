d9b0b74f46243e248ff5a9c4b835ae2a99ca6a7c
RDoneux <robertdoneux@gmail.com>
2024-04-07 09:47:45 +0100

    enables upload of post images to DB

--- src/app/components/rich-text-input/rich-text-input.component.ts
+++ src/app/components/rich-text-input/rich-text-input.component.ts
@@ -6,7 +6,8 @@ import {
   ViewChild,
 } from '@angular/core';
 import Quill from 'quill';
-import { UtilsService } from 'src/app/services/utils/utils.service';
+import { finalize } from 'rxjs';
+import { StorageService } from 'src/app/services/cloud-storage/storage.service';
 
 @Component({
   selector: 'app-rich-text-input',
@@ -20,15 +21,18 @@ export class RichTextInputComponent implements AfterViewInit {
   @Input() text: string = '{}'; // Delta object stringified
   @Input() mode: 'edit' | 'display' = 'edit';
 
+  private readonly DB_DIRECTORY: string = 'post-images';
+  private readonly LOADING_ANIMATION_PATH: string =
+    'assets/icons/loading-animation.gif';
+  private quillInput!: Quill;
+
   @ViewChild('content')
   private _content!: ElementRef;
   public get content(): HTMLDivElement {
     return this._content.nativeElement;
   }
 
-  private quillInput!: Quill;
-
-  constructor() {}
+  constructor(private storageService: StorageService) {}
 
   ngAfterViewInit(): void {
     let options: any = {
@@ -51,10 +55,76 @@ export class RichTextInputComponent implements AfterViewInit {
 
     this.quillInput = new Quill(this.content, options);
     this.quillInput.setContents(JSON.parse(this.text));
+    if (this.mode === 'edit') {
+      this.quillInput
+        .getModule('toolbar')
+        .addHandler('image', () => this.imageUploaded());
+    }
     if (this.mode === 'display') this.quillInput.disable();
   }
 
   getContent(): string {
     return JSON.stringify(this.quillInput.getContents());
+  }
+
+  // IMAGE UPLOAD TO SERVER LOGIC //
+
+  imageUploaded(): void {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.click();
+
+    // // Listen upload local image and save to server
+    input.onchange = () => {
+      if (!input.files) return;
+      const file = input.files[0];
+      // only allow image uploads.
+      if (/^image\//.test(file.type)) {
+        this.saveToFirebase(file);
+      } else {
+        console.warn('You could only upload images.');
+      }
+    };
+  }
+
+  saveToFirebase(file: File): void {
+    this.insertLoadingAnimation();
+
+    const randomId = Math.random().toString(36).substring(2);
+    const ref = this.storageService
+      .getStorage()
+      .ref(`${this.DB_DIRECTORY}/${randomId}`);
+
+    const task = ref.put(file);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          ref.getDownloadURL().subscribe((url) => {
+            this.insertToEditor(url);
+          });
+        })
+      )
+      .subscribe();
+  }
+
+  private loadingAnimationLocation!: number;
+  insertLoadingAnimation(): void {
+    const range = this.quillInput.getSelection();
+    if (!range) return;
+    this.loadingAnimationLocation = range.index;
+    this.quillInput.insertEmbed(
+      range.index,
+      'image',
+      this.LOADING_ANIMATION_PATH
+    );
+  }
+
+  insertToEditor(url: string) {
+    // push image url to rich editor.
+    const range = this.quillInput.getSelection();
+    if (!range) return;
+    this.quillInput.deleteText(this.loadingAnimationLocation, 1); // remove the loading animation
+    this.quillInput.insertEmbed(range.index, 'image', url);
   }
 }
